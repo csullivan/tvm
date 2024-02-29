877970064dd349e93eb36a3528c33e9af2b59b95
Wuwei Lin <wuwei@apache.org>
2024-02-29 19:14:37 +0000

    add e5m2_e4m3 and e5m2_e5m2

--- src/runtime/contrib/cutlass/fp8_group_gemm.cu
+++ src/runtime/contrib/cutlass/fp8_group_gemm.cu
@@ -57,72 +57,15 @@
 
 using namespace cute;
 using ProblemShape = cutlass::gemm::GroupProblemShape<Shape<int, int, int>>;  // <M,N,K> per group
-using ElementA = cutlass::float_e4m3_t;  // Element type for A matrix operand
-using ElementB = cutlass::float_e4m3_t;  // Element type for B matrix operand
-using ElementC = cutlass::float_e4m3_t;  // Element type for C and D matrix operands
 
 #if defined(CUTLASS_ARCH_MMA_MODIFIABLE_TMA_SM90_SUPPORTED)
-
-/////////////////////////////////////////////////////////////////////////////////////////////////
-/// GEMM kernel configurations
-/////////////////////////////////////////////////////////////////////////////////////////////////
-
-// A matrix configuration
-using LayoutA = cutlass::layout::RowMajor;  // Layout type for A matrix operand
-constexpr int AlignmentA =
-    128 / cutlass::sizeof_bits<ElementA>::value;  // Alignment of A matrix in units of elements (up
-                                                  // to 16 bytes)
-
-// B matrix configuration
-using LayoutB = cutlass::layout::ColumnMajor;  // Layout type for B matrix operand
-constexpr int AlignmentB =
-    128 / cutlass::sizeof_bits<ElementB>::value;  // Alignment of B matrix in units of elements (up
-                                                  // to 16 bytes)
-
-// C/D matrix configuration
-using LayoutC = cutlass::layout::RowMajor;  // Layout type for C and D matrix operands
-constexpr int AlignmentC =
-    128 / cutlass::sizeof_bits<ElementC>::value;  // Alignment of C matrix in units of elements (up
-                                                  // to 16 bytes)
-
-// Core kernel configurations
-using ElementAccumulator = float;  // Element type for internal accumulation
-using ArchTag =
-    cutlass::arch::Sm90;  // Tag indicating the minimum SM that supports the intended feature
-using OperatorClass = cutlass::arch::OpClassTensorOp;  // Operator class tag
-using TileShape = Shape<_256, _128, _64>;              // Threadblock-level tile size
-using ClusterShape = Shape<_2, _2, _1>;                // Shape of the threadblocks in a cluster
-using StageCountType =
-    cutlass::gemm::collective::StageCountAuto;  // Stage count maximized based on the tile size
-using KernelSchedule =
-    cutlass::gemm::KernelPtrArrayTmaWarpSpecializedCooperativeFP8FastAccum;  // Kernel to launch
-using EpilogueSchedule = cutlass::epilogue::PtrArrayNoSmemWarpSpecialized;   // Epilogue to launch
-
-using CollectiveEpilogue = typename cutlass::epilogue::collective::CollectiveBuilder<
-    cutlass::arch::Sm90, cutlass::arch::OpClassTensorOp, TileShape, ClusterShape,
-    cutlass::epilogue::collective::EpilogueTileAuto, ElementAccumulator, ElementAccumulator,
-    ElementC, LayoutC*, AlignmentC, ElementC, LayoutC*, AlignmentC, EpilogueSchedule>::CollectiveOp;
-
-using CollectiveMainloop = typename cutlass::gemm::collective::CollectiveBuilder<
-    ArchTag, OperatorClass, ElementA, LayoutA*, AlignmentA, ElementB, LayoutB*, AlignmentB,
-    ElementAccumulator, TileShape, ClusterShape,
-    cutlass::gemm::collective::StageCountAutoCarveout<static_cast<int>(
-        sizeof(typename CollectiveEpilogue::SharedStorage))>,
-    KernelSchedule>::CollectiveOp;
-
-using GemmKernel =
-    cutlass::gemm::kernel::GemmUniversal<ProblemShape, CollectiveMainloop, CollectiveEpilogue>;
-
-using Gemm = cutlass::gemm::device::GemmUniversalAdapter<GemmKernel>;
-
-using StrideA = typename Gemm::GemmKernel::UnderlyingStrideA;
-using StrideB = typename Gemm::GemmKernel::UnderlyingStrideB;
-using StrideC = typename Gemm::GemmKernel::UnderlyingStrideC;
-using StrideD = typename Gemm::GemmKernel::UnderlyingStrideD;
 
 inline size_t aligned(size_t value, size_t alignment = 16) {
   return (value + alignment - 1) / alignment * alignment;
 }
+
+template <typename ElementA, typename ElementB, typename ElementC, typename StrideA,
+          typename StrideB, typename StrideC>
 __global__ void prepare_group_gemm_arguments(
     const ElementA** ptr_A, const ElementB** ptr_B, ElementC** ptr_D,
     typename ProblemShape::UnderlyingProblemShape* problem_sizes, StrideA* stride_A,
@@ -141,11 +84,66 @@ __global__ void prepare_group_gemm_arguments(
   stride_D[expert_id] = cute::make_stride(n, Int<1>{}, int64_t{0});
 }
 
+template <typename ElementA, typename ElementB, typename ElementC>
 void cutlass_fp8_moe_gemm(ElementA* x, ElementB* weight, int64_t* total_rows_before_expert,
                           uint8_t* workspace, int64_t total_rows, int64_t n, int64_t k,
                           int64_t num_experts, ElementC* out, cudaStream_t stream) {
-  Gemm::EpilogueOutputOp::Params epilogue_params{ElementAccumulator(1.0f),
-                                                 ElementAccumulator(0.0f)};
+  // A matrix configuration
+  using LayoutA = cutlass::layout::RowMajor;  // Layout type for A matrix operand
+  constexpr int AlignmentA =
+      128 / cutlass::sizeof_bits<ElementA>::value;  // Alignment of A matrix in units of elements
+                                                    // (up to 16 bytes)
+
+  // B matrix configuration
+  using LayoutB = cutlass::layout::ColumnMajor;  // Layout type for B matrix operand
+  constexpr int AlignmentB =
+      128 / cutlass::sizeof_bits<ElementB>::value;  // Alignment of B matrix in units of elements
+                                                    // (up to 16 bytes)
+
+  // C/D matrix configuration
+  using LayoutC = cutlass::layout::RowMajor;  // Layout type for C and D matrix operands
+  constexpr int AlignmentC =
+      128 / cutlass::sizeof_bits<ElementC>::value;  // Alignment of C matrix in units of elements
+                                                    // (up to 16 bytes)
+
+  // Core kernel configurations
+  using ElementAccumulator = float;  // Element type for internal accumulation
+  using ArchTag =
+      cutlass::arch::Sm90;  // Tag indicating the minimum SM that supports the intended feature
+  using OperatorClass = cutlass::arch::OpClassTensorOp;  // Operator class tag
+  using TileShape = Shape<_256, _128, _64>;              // Threadblock-level tile size
+  using ClusterShape = Shape<_2, _2, _1>;                // Shape of the threadblocks in a cluster
+  using StageCountType =
+      cutlass::gemm::collective::StageCountAuto;  // Stage count maximized based on the tile size
+  using KernelSchedule =
+      cutlass::gemm::KernelPtrArrayTmaWarpSpecializedCooperativeFP8FastAccum;  // Kernel to launch
+  using EpilogueSchedule = cutlass::epilogue::PtrArrayNoSmemWarpSpecialized;   // Epilogue to launch
+
+  using CollectiveEpilogue = typename cutlass::epilogue::collective::CollectiveBuilder<
+      cutlass::arch::Sm90, cutlass::arch::OpClassTensorOp, TileShape, ClusterShape,
+      cutlass::epilogue::collective::EpilogueTileAuto, ElementAccumulator, ElementAccumulator,
+      ElementC, LayoutC*, AlignmentC, ElementC, LayoutC*, AlignmentC,
+      EpilogueSchedule>::CollectiveOp;
+
+  using CollectiveMainloop = typename cutlass::gemm::collective::CollectiveBuilder<
+      ArchTag, OperatorClass, ElementA, LayoutA*, AlignmentA, ElementB, LayoutB*, AlignmentB,
+      ElementAccumulator, TileShape, ClusterShape,
+      cutlass::gemm::collective::StageCountAutoCarveout<static_cast<int>(
+          sizeof(typename CollectiveEpilogue::SharedStorage))>,
+      KernelSchedule>::CollectiveOp;
+
+  using GemmKernel =
+      cutlass::gemm::kernel::GemmUniversal<ProblemShape, CollectiveMainloop, CollectiveEpilogue>;
+
+  using Gemm = cutlass::gemm::device::GemmUniversalAdapter<GemmKernel>;
+
+  using StrideA = typename Gemm::GemmKernel::UnderlyingStrideA;
+  using StrideB = typename Gemm::GemmKernel::UnderlyingStrideB;
+  using StrideC = typename Gemm::GemmKernel::UnderlyingStrideC;
+  using StrideD = typename Gemm::GemmKernel::UnderlyingStrideD;
+
+  typename Gemm::EpilogueOutputOp::Params epilogue_params{ElementAccumulator(1.0f),
+                                                          ElementAccumulator(0.0f)};
   // set up pointers
   std::ptrdiff_t offset = 0;
   const ElementA** ptr_A = reinterpret_cast<const ElementA**>(workspace + offset);
@@ -189,21 +187,33 @@ void cutlass_fp8_moe_gemm(ElementA* x, ElementB* weight, int64_t* total_rows_bef
 namespace tvm {
 namespace runtime {
 
-TVM_REGISTER_GLOBAL("cutlass.moe_gemm_f8f8")
-    .set_body_typed([](NDArray x, NDArray weight, NDArray total_rows_before_expert,
-                       NDArray workspace, int64_t total_rows, int64_t n, int64_t k,
-                       int64_t num_experts, NDArray out) {
-      // workspace: currently only used to store the parameters for ther kernel, it is not used as
-      // the workspace for the cutlass internal computation. Workspace of 4kB is sufficient for the
-      // parameters.
-      auto func = tvm::runtime::Registry::Get("runtime.get_cuda_stream");
-      ICHECK(func != nullptr);
-      cudaStream_t stream = static_cast<cudaStream_t>((*func)().operator void*());
-      cutlass_fp8_moe_gemm(static_cast<ElementA*>(x->data), static_cast<ElementB*>(weight->data),
-                           static_cast<int64_t*>(total_rows_before_expert->data),
-                           static_cast<uint8_t*>(workspace->data), total_rows, n, k, num_experts,
-                           static_cast<ElementC*>(out->data), stream);
-    });
+template <typename ElementA, typename ElementB, typename ElementC>
+void tvm_cutlass_fp8_moe_gemm(NDArray x, NDArray weight, NDArray total_rows_before_expert,
+                              NDArray workspace, int64_t total_rows, int64_t n, int64_t k,
+                              int64_t num_experts, NDArray out) {
+  // workspace: currently only used to store the parameters for ther kernel, it is not used as
+  // the workspace for the cutlass internal computation. Workspace of 4kB is sufficient for the
+  // parameters.
+  auto func = tvm::runtime::Registry::Get("runtime.get_cuda_stream");
+  ICHECK(func != nullptr);
+  cudaStream_t stream = static_cast<cudaStream_t>((*func)().operator void*());
+  cutlass_fp8_moe_gemm(static_cast<ElementA*>(x->data), static_cast<ElementB*>(weight->data),
+                       static_cast<int64_t*>(total_rows_before_expert->data),
+                       static_cast<uint8_t*>(workspace->data), total_rows, n, k, num_experts,
+                       static_cast<ElementC*>(out->data), stream);
+}
+
+TVM_REGISTER_GLOBAL("cutlass.moe_gemm_e5m2_e5m2_fp16")
+    .set_body_typed(
+        tvm_cutlass_fp8_moe_gemm<cutlass::float_e5m2_t, cutlass::float_e5m2_t, cutlass::half_t>);
+
+TVM_REGISTER_GLOBAL("cutlass.moe_gemm_e5m2_e4m3_fp16")
+    .set_body_typed(
+        tvm_cutlass_fp8_moe_gemm<cutlass::float_e5m2_t, cutlass::float_e4m3_t, cutlass::half_t>);
+
+TVM_REGISTER_GLOBAL("cutlass.moe_gemm_e4m3_e4m3_fp16")
+    .set_body_typed(
+        tvm_cutlass_fp8_moe_gemm<cutlass::float_e4m3_t, cutlass::float_e4m3_t, cutlass::half_t>);
 
 }  // namespace runtime
 }  // namespace tvm
